3a8cac5369cc3a7231bff25b80b3602cfbfb7529
JavierMCap <javier.dezkauriatza@momentocapital.com>
2024-08-23 09:58:42 -0600

    Update bbands_app_finaltest.py

--- bbands_app_finaltest.py
+++ bbands_app_finaltest.py
@@ -182,7 +182,7 @@ zscore_date = extract_date_from_filename(z_score_excel_file_path)
 
 
 # Load the image from a URL or a local file
-image_url = "C:\\Users\\jabo\\Desktop\\finance_courses\\momento_logo.png"  # Update this URL
+image_url = "momento_logo.png"  # Update this URL
 image = Image.open(image_url)
 
 # Display the logo in the sidebar

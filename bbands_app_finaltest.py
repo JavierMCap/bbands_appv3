6fd557b03d8549d678a398603b95a85265ec1017
JavierMCap <javier.dezkauriatza@momentocapital.com>
2024-10-08 18:19:07 -0600

    Update bbands_app_finaltest.py

--- bbands_app_finaltest.py
+++ bbands_app_finaltest.py
@@ -429,10 +429,10 @@ if selected_analysis == "BBands analysis":
         
         # Display the top 5 highest and lowest correlations
         st.subheader(f"5 Highest Correlations for {selected_ticker}")
-        st.dataframe(highest_5)
+        st.dataframe(lowest_5)
 
         st.subheader(f"5 Lowest Correlations for {selected_ticker}")
-        st.dataframe(lowest_5)
+        st.dataframe(highest_5)
 
     # Perform and display the analysis for the selected ticker
     symbol, current_price, today_percentage, five_day_percentage, mtd_percentage, qtd_percentage, ytd_percentage = analyze_symbol(selected_ticker, api_token)
